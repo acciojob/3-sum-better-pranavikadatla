6fd575b535b685c317061aeee1d3a25ac8b2568f
pranavikadatla <142671406+pranavikadatla@users.noreply.github.com>
2024-07-10 15:04:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,28 @@ function threeSum(arr, target) {
 // write your code here
 	 let closestSum = Infinity;
 
-    for (let i = 0; i < arr.length - 2; i++) {
-        let left = 0, right = arr.length - 1;
+    for (let i = 1; i < arr.length; i++) {
         while (left < right) {
-            let currentSum = arr[i] + arr[left] + arr[right];
+            let currentSum = arr[i-1] + arr[i] + arr[i]
+    // If this sum is closer to the target than the previous closest sum
+            if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+                closestSum = currentSum;
+            }
+            
+            // Adjust the pointers
+            if (currentSum < target) {
+                left++;
+            } else if (currentSum > target) {
+                right--;
+            } else {
+                // If the current sum is exactly equal to the target
+                return currentSum;
+            }
+        }
+    }
+
+    return closestSum;
+}];
             
             // If this sum is closer to the target than the previous closest sum
             if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
